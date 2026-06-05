9b1967f96b1bc210c6e1d11a191b553cedd95fbb
van-gogh <weiqifan0524@gmail.com>
2026-06-06 07:55:24 +0800

    day12.0; Jun-6

---
+++ Chapter_14/adder/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let num =10;
+    println!("The value of num is {}, plus one is {}", num, add_one::add_one(num));
+}
--- Chapter_7/src/main.rs
+++ Chapter_7/src/main.rs
@@ -1,6 +1,21 @@
+fn insertion_sort(arr: &mut [i32]) {
+    for j in 1..arr.len() {
+        let key = arr[j];
+        let mut i = j;
 
-use rand::Rng;
-fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+        while i > 0 && arr[i - 1] > key {
+            arr[i] = arr[i - 1];
+            i -= 1;
+        }
+
+        arr[i] = key;
+    }
 }
 
+fn main() {
+    let mut arr = vec![5, 2, 4, 6, 1, 3];
+
+    insertion_sort(&mut arr);
+
+    println!("{:?}", arr);
+}
