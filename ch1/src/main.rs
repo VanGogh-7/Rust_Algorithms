86e49823d6664b20fd83dfa18c78eebc9d1d091d
van-gogh <weiqifan0524@gmail.com>
2026-06-29 20:06:39 +0800

    day15.0; Jun-29

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -1,5 +1,18 @@
+struct Solution;
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let n = n as usize;
+        let mut result = Vec::with_capacity(2 * n);
+
+        for i in 0..n {
+            result.push(nums[i]);
+            result.push(nums[i + n]);
+        }
+        result
+    }
+}
 fn main() {
-    let x: i32 = 10;
-    println!("x = {}", x);
-    println!("Hello, world!");
+    let nums = vec![1, 2, 3, 4];
+    let n: Vec<i32> = Solution::shuffle(nums, 2);
+    println!("{:?}", n);
 }
